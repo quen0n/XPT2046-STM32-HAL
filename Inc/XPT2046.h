dc299a7a8556e6b18d9d4ef690dc4cb121bcf349
Victor Nikitchuk <rainbowkiwifox@gmail.com>
2020-03-30 00:21:24 +0300

    Допилина функция контроля работы SPI с автовозвратом предыдущих значений

--- Inc/XPT2046.h
+++ Inc/XPT2046.h
@@ -16,32 +16,40 @@
 #define XPT2046_H_
 #include "main.h"
 
+/* Параметры SPI */
+//Включить контроль скорости работы SPI
+//Если включено, то скорость SPI будет автоматически переключаться на безопасную
 #define XPT2046_SPI_PARAM_CONTROL
+//Делитель частоты работы SPI
+#define XPT2046_SPI_PRESCALER SPI_BAUDRATEPRESCALER_64 
 
-
-// Warning! Use SPI bus with < 2.5 Mbit speed, better ~650 Kbit to be save.
-#define XPT2046_SPI_PORT hspi1
-extern SPI_HandleTypeDef XPT2046_SPI_PORT;
-
+/* Порты работы тачскрина */
+//Порт прерывания должен быть подтянут к питанию!
 #define XPT2046_IRQ_Pin       T_IRQ_Pin
 #define XPT2046_IRQ_GPIO_Port T_IRQ_GPIO_Port
 #define XPT2046_CS_Pin        T_CS_Pin
 #define XPT2046_CS_GPIO_Port  T_CS_GPIO_Port
 
-// change depending on screen orientation
+/* Параметры ориентации */
+//TODO: Сделать установку параметров при инициализации
 #define XPT2046_SCALE_X 320
 #define XPT2046_SCALE_Y 240
 
-// to calibrate uncomment UART_Printf line in ili9341_touch.c
+/* Калибровочные значения дисплея */
 #define XPT2046_MIN_RAW_X 1500
 #define XPT2046_MAX_RAW_X 30000
 #define XPT2046_MIN_RAW_Y 1800
 #define XPT2046_MAX_RAW_Y 29000
 
-// call before initializing any SPI devices
-uint8_t XPT2046_TouchPressed(void);
+/* Прототипы функций */
+//Функция инициализации тачскрина
+//В аргументе указывается интерфейс SPI
+void XPT2046_init(SPI_HandleTypeDef *spi);
+//Проверка нажатия на экран. Возвращает истину если есть прикосновение
+uint8_t XPT2046_TouchPressed(void); 
+//Получить координаты нажатия
+//TODO: Сделать нормальный обработчик нажатия на экран с прерываниями и без
+//TODO: Возвращать объект с координатами
 uint8_t XPT2046_TouchGetCoordinates(uint16_t* x, uint16_t* y);
 
-
-
-#endif /* XPT2046_TOUCH_H_ */
+#endif /* XPT2046_H_ */
--- Src/XPT2046.c
+++ Src/XPT2046.c
@@ -12,27 +12,31 @@
 *========================================================================================================
 */
 
-#include <stdio.h>
 #include "XPT2046.h"
 
 #define READ_X 0x90
 #define READ_Y 0xD0
 
+//Указатель на интерфейс SPI
+SPI_HandleTypeDef *_spi;
 
+//Применение безопасных параметров SPI
+//TODO: Автоопределение скорости SPI
 #ifdef XPT2046_SPI_PARAM_CONTROL
-static void _spi_init(SPI_HandleTypeDef *spi) {
-  spi->Init.Mode = SPI_MODE_MASTER;
-  spi->Init.Direction = SPI_DIRECTION_2LINES;
-  spi->Init.DataSize = SPI_DATASIZE_8BIT;
-  spi->Init.CLKPolarity = SPI_POLARITY_LOW;
-  spi->Init.CLKPhase = SPI_PHASE_1EDGE;
-  spi->Init.NSS = SPI_NSS_SOFT;
-  spi->Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
-  spi->Init.FirstBit = SPI_FIRSTBIT_MSB;
-  spi->Init.TIMode = SPI_TIMODE_DISABLE;
-  spi->Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  spi->Init.CRCPolynomial = 10;
-  if (HAL_SPI_Init(spi) != HAL_OK)
+static void _spi_init(void) {
+  _spi->Init.Mode = SPI_MODE_MASTER;
+  _spi->Init.Direction = SPI_DIRECTION_2LINES;
+  _spi->Init.DataSize = SPI_DATASIZE_8BIT;
+  _spi->Init.CLKPolarity = SPI_POLARITY_LOW;
+  _spi->Init.CLKPhase = SPI_PHASE_1EDGE;
+  _spi->Init.NSS = SPI_NSS_SOFT;
+	//Делитель частоты SPI. Установите безопасное значение в XPT2046.h
+  _spi->Init.BaudRatePrescaler = XPT2046_SPI_PRESCALER;
+  _spi->Init.FirstBit = SPI_FIRSTBIT_MSB;
+  _spi->Init.TIMode = SPI_TIMODE_DISABLE;
+  _spi->Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  _spi->Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(_spi) != HAL_OK)
   {
     Error_Handler();
   }
@@ -53,11 +57,19 @@ uint8_t XPT2046_TouchPressed(void)
 {
     return HAL_GPIO_ReadPin(XPT2046_IRQ_GPIO_Port, XPT2046_IRQ_Pin) == GPIO_PIN_RESET;
 }
+//Функция инициализации тачскрина
+//В аргументе указывается интерфейс SPI
+void XPT2046_init(SPI_HandleTypeDef *spi) {
+	_spi = spi;
+}
 
+//Функция получения координат нажатия на экран
 uint8_t XPT2046_TouchGetCoordinates(uint16_t* x, uint16_t* y)
 {
+	//Если включен контроль скорости SPI, то сохранение параметров и установка правильных значений
 	#ifdef XPT2046_SPI_PARAM_CONTROL
-		_spi_init(&XPT2046_SPI_PORT);
+	SPI_HandleTypeDef old_spi = *_spi;//Сохранение старых параметров SPI
+	_spi_init(); //Инициализация с правильными параметрами
 	#endif
     static const uint8_t cmd_read_x[] = { READ_X };
     static const uint8_t cmd_read_y[] = { READ_Y };
@@ -76,13 +88,13 @@ uint8_t XPT2046_TouchGetCoordinates(uint16_t* x, uint16_t* y)
 
         nsamples++;
 
-        HAL_SPI_Transmit(&XPT2046_SPI_PORT, (uint8_t*)cmd_read_y, sizeof(cmd_read_y), HAL_MAX_DELAY);
+        HAL_SPI_Transmit(_spi, (uint8_t*)cmd_read_y, sizeof(cmd_read_y), HAL_MAX_DELAY);
         uint8_t y_raw[2];
-        HAL_SPI_TransmitReceive(&XPT2046_SPI_PORT, (uint8_t*)zeroes_tx, y_raw, sizeof(y_raw), HAL_MAX_DELAY);
+        HAL_SPI_TransmitReceive(_spi, (uint8_t*)zeroes_tx, y_raw, sizeof(y_raw), HAL_MAX_DELAY);
 
-        HAL_SPI_Transmit(&XPT2046_SPI_PORT, (uint8_t*)cmd_read_x, sizeof(cmd_read_x), HAL_MAX_DELAY);
+        HAL_SPI_Transmit(_spi, (uint8_t*)cmd_read_x, sizeof(cmd_read_x), HAL_MAX_DELAY);
         uint8_t x_raw[2];
-        HAL_SPI_TransmitReceive(&XPT2046_SPI_PORT, (uint8_t*)zeroes_tx, x_raw, sizeof(x_raw), HAL_MAX_DELAY);
+        HAL_SPI_TransmitReceive(_spi, (uint8_t*)zeroes_tx, x_raw, sizeof(x_raw), HAL_MAX_DELAY);
 
         avg_x += (((uint16_t)x_raw[0]) << 8) | ((uint16_t)x_raw[1]);
         avg_y += (((uint16_t)y_raw[0]) << 8) | ((uint16_t)y_raw[1]);
@@ -108,6 +120,14 @@ uint8_t XPT2046_TouchGetCoordinates(uint16_t* x, uint16_t* y)
 
     *x = (raw_x - XPT2046_MIN_RAW_X) * XPT2046_SCALE_X / (XPT2046_MAX_RAW_X - XPT2046_MIN_RAW_X);
     *y = (raw_y - XPT2046_MIN_RAW_Y) * XPT2046_SCALE_Y / (XPT2046_MAX_RAW_Y - XPT2046_MIN_RAW_Y);
-
-    return 1;
+	//Восстановление старых параметров
+	#ifdef XPT2046_SPI_PARAM_CONTROL
+	*_spi = old_spi; 
+	//Инициализация с старыми параметрами
+  if (HAL_SPI_Init(_spi) != HAL_OK)
+  {
+    Error_Handler();
+  }
+	#endif
+	return 1;
 }
